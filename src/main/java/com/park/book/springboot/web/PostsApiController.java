4066b5bc9b02ae0a229bdec4e8806b3f88da7390
jwpark7414 <jwpark7414>
2020-12-09 22:15:55 +0900

    Posts CUR 추가

---
+++ src/main/java/com/park/book/springboot/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.park.book.springboot.web;
+
+import com.park.book.springboot.service.posts.PostsService;
+import com.park.book.springboot.web.dto.PostsResponseDto;
+import com.park.book.springboot.web.dto.PostsSaveRequestDto;
+import com.park.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable long id) {
+        return postsService.findById(id);
+    }
+}
