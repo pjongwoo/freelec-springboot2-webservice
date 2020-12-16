1ae8a74ea571f5e31b3591e86b1d2b9f4e3ef40b
jwpark7414 <jwpark7414>
2020-12-16 14:26:10 +0900

    TEST 추가

--- src/test/java/com/park/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/park/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class HelloResponseDtoTest {
 
     @Test
-    public void lombok_action_test() {
+    public void 롬복_기능_테스트() {
         //given
         String name = "test";
         int amount = 1000;
